587c203cf69fb13532ecb7a829b68806a8bcf504
Maria Jazmin <jazminbarreto19@gmail.com>
2023-08-24 20:24:22 -0400

    flask mysql

--- flask_mysql/clases_profe/flask_app/models/subcategoria.py
+++ flask_mysql/clases_profe/flask_app/models/subcategoria.py
@@ -1,14 +1,21 @@
-from hola_flask.config.mysqlconnections import connectToMySQL
-from categoria import Categoria
+from flask_app.config.mysqlconnections import connectToMySQL
+from flask_app.models import categoria
 
 class Subcategoria:
     def __init__(self, data) -> None:
         self.id = data['id']
         self.nombre = data['nombre']
-        self.categoria = None
+        self.categoria = data.get("categoria")
 
     def __str__(self) -> str:
         return f"Instancia de Subcategoria {self.nombre} con ID {self.id}"
+    
+    #validación de los datos que se reciben
+    @staticmethod
+    def validar(datos_formulario)
+        resultado = True
+
+        return resultado
 
     @classmethod
     def get_all(cls):
@@ -22,7 +29,7 @@ class Subcategoria:
                 'id': resultado['categorias.id'],
                 'nombre': resultado['categorias.nombre'],
             }
-            instancia_categoria = Categoria(datos_categoria)
+            instancia_categoria = categoria.Categoria(datos_categoria)
             instancia.categoria = instancia_categoria
 
             resultados_instancias.append(instancia)
--- flask_mysql/clases_profe/tempCodeRunnerFile.py
+++ flask_mysql/clases_profe/tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, flash
-from hola_flask.config.mysqlconnections import connectToMySQL
+from flask_app.config.mysqlconnections import connectToMySQL
 app = Flask(__name__)
 app.secret_key = 'cualquier cosa'
 
